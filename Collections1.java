36cf8d6ee625105cd018604bed0252ff1a0c066e
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-27 18:36:41 +0200

    Update Collections1.java

--- Collections1.java
+++ Collections1.java
@@ -1,31 +1,29 @@
 package collections1;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Collections1 {
 
     public static void main(String[] args) {
-        System.out.println("please enter a number: ");
-        ArrayList<Integer> nums = new ArrayList<>();
-        int count[] = new int[10];
+        int count[] = new int[9];
+        TreeSet<Integer> set = new TreeSet<>();
         Scanner t = new Scanner(System.in);
         int num = 1;
         for (int i = 0; num != 0; i++) {
-            System.out.print("enter another number or enter 0 to finish");
+            System.out.print("enter a number or enter 0 to finish: ");
             num = t.nextInt();
             if (num != 0) {
-                nums.add(num);
-                count[num] = count[num] +1;
+                set.add(num);
+                count[num - 1] = count[num - 1] + 1;
             } else {
                 break;
             }
         }
-        TreeSet<Integer> set = new TreeSet<>(nums);
         System.out.println(set);
-         for (int i = 0; i < 10; i++) 
-            System.out.println(i + " .... > " + count[i]);
+        for (int i = 0; i < 9; i++) {
+            System.out.println((i + 1) + " .... > " + count[i]);
         }
-
     }
+
+}
