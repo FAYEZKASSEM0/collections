34038767b401b1312ac34ea0e195a9e44da3f23e
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-27 20:20:40 +0200

    Update Collections3.java

--- Collections3.java
+++ Collections3.java
@@ -1,29 +1,21 @@
-package collections.pkg3;
+package test2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Collections3 {
+public class Test2 {
 
     public static void main(String[] args) {
         System.out.print("please enter the round brackets: ");
         Scanner t = new Scanner(System.in);
-        String brackets = t.nextLine();
-        ArrayList<String> bracketsarr = new ArrayList(Arrays.asList(brackets.split("")));
-        int size= bracketsarr.size();
-        if (size % 2 == 1) {
-            System.out.print(false);
-        } else {
-            for (int i = 0; bracketsarr.contains("("); i++) {
-                bracketsarr.remove("(");
-            }
-            if (bracketsarr.size() == size/ 2) {
-                System.out.println(true);
-            } else {
-                System.out.println(false);
-            }
-
-        }
+        String bracket = t.nextLine();
+        ArrayList<String> brackets = new ArrayList(Arrays.asList(bracket.split("")));
+        int r = Collections.frequency(brackets, "("), l= Collections.frequency(brackets, ")");
+        if (r==l)
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
