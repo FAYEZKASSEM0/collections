1883906e04f62ae654aa2ac289962a6d4e36b752
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-28 23:44:46 +0200

    Update Collectios2.java

--- Collectios2.java
+++ Collectios2.java
@@ -1,42 +1,47 @@
-package collectios2;
+package test2;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Collectios2 {
+public class Test2 {
+    static HashMap<String, Integer> names = new HashMap<>();
+    static String name = null;
+    static int employeenum = 0;
+    static Scanner t = new Scanner(System.in);
+    static Scanner z = new Scanner(System.in);
 
+    
+    static int ifthere(String name) {
+        System.out.print("please enter the payment value: ");
+        int pay = t.nextInt();
+        int prepay = names.get(name);
+        return pay + prepay;
+    }
+    
     public static void main(String[] args) {
         System.out.println("how many employee do you have? ");
-        Scanner t = new Scanner(System.in);
-        Scanner z = new Scanner(System.in);
+        employeenum = t.nextInt();
 
-        int employeenum = t.nextInt();
-        HashMap<String, Integer> names = new HashMap<>();
-        String name = null;
         for (int i = 0; i < employeenum; i++) {
             System.out.print("enter an employee name: ");
             name = z.nextLine();
             names.put(name, 0);
         }
-
-        for (int i = 0; !"0".equals(name); i++) {
-            System.out.println("please enter the employee name: ");
+        
+        System.out.println("please enter the employee name: ");
+        
+        for (; ; ) {
             name = z.nextLine();
-            if ("0".equals(name)) {
+            if ("0".equals(name)) 
                 break;
-            }
-            if (names.containsKey(name)) {
-                System.out.print("please enter the payment value: ");
-                int pay = t.nextInt();
-                int prepay = names.get(name), totalpay = pay + prepay;
-                names.put(name, totalpay);
-                System.out.println("if you want another operation enter the name of the employee, if you don't enter \"0\"");
-            } else {
+            else if (names.containsKey(name)) 
+                names.put(name, ifthere(name));
+            else 
                 System.out.println("you entered a wrong name please try again");
-            }
+            
+            System.out.println("if you want another operation enter the name of the employee, if you don't enter \"0\"");
         }
+
         System.out.println(names);
     }
-
 }
